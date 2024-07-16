251ac584768aad34d9c23a54fa3e65866a9ed19c
samuelgsrmgc <99485171+samuelgsrmgc@users.noreply.github.com>
2024-07-16 11:04:03 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,18 +6,13 @@ addEventListener("fetch", (event) => {
 const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
-  // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
-
-  // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker.mydomain.com": "https://registry-1.docker.io",
+  "quay.mydomain.com": "https://quay.io",
+  "gcr.mydomain.com": "https://gcr.io",
+  "k8s-gcr.mydomain.com": "https://k8s.gcr.io",
+  "k8s.mydomain.com": "https://registry.k8s.io",
+  "ghcr.mydomain.com": "https://ghcr.io",
+  "cloudsmith.mydomain.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
